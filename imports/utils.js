74547689e7749f680f964e5d3812f208a109f392
mukesh <mukesh.bhatia85@gmail.com>
2018-10-16 20:39:04 +0530

    Initial commit

---
+++ imports/utils.js
@@ -0,0 +1,4 @@
+console.log('Log From /imports/utils.js');
+export let greetuser = function() {
+  return 'Hello User';
+};
---
+++ server/main.js
@@ -0,0 +1,5 @@
+//import './../imports/utils';
+import add from './../imports/math';
+
+console.log('Log from /server/main.js');
+console.log(add (9,3));
